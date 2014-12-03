c259e9f9c765a18441c58679f63a27c45b6665f7
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2014-12-03 13:14:53 +0100

    Update elevator.vhdl
    verision 1.0
    
    Not testes with testbench

--- elevator.vhdl
+++ elevator.vhdl
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -42,9 +42,9 @@ entity TOP is
            i_RST : in  STD_LOGIC; --Resetsignal - beim einschalten kurzzeiteig auf 1 __WIESO?
            CLK : in  STD_LOGIC; -- Systemtakt: 1MHz
            o_MOTOR_DIR : out  STD_LOGIC; --Richtungsangabe 1 -> hoch 0 -> runter
-           o_MOTOR_EN : in  STD_LOGIC; --Motor an/aus
-           o_DOOR_DIR : in  STD_LOGIC; --Tür richtung 1 -> schließen , 0 -> öffnen
-           o_DOOR_EN : in  STD_LOGIC); --Tür öffnen/schließen
+           o_MOTOR_EN : out  STD_LOGIC; --Motor an/aus
+           o_DOOR_DIR : out  STD_LOGIC; --Tür richtung 1 -> schließen , 0 -> öffnen
+           o_DOOR_EN : out  STD_LOGIC); --Tür öffnen/schließen
 end TOP;
 
 architecture Behavioral of TOP is
@@ -73,6 +73,7 @@ END COMPONENT;
 
 
 signal DATA_BUS : std_logic_vector(7 downto 0);
+signal REQUEST: std_logic_vector(3 downto 0);
 signal CLK_DOG : STD_LOGIC;
 signal DOG_OUT : STD_LOGIC;
 signal RELOAD : STD_LOGIC;
@@ -81,6 +82,9 @@ signal STRB: STD_LOGIC;
 
 type STATE_TYPE is(drive_up, drive_down, stay);
 signal STATE, NEXT_ST: STATE_TYPE;
+
+type STATE_DOOR is(door_opening, door_closing, door_open, door_shut);
+signal DOOR,NEXT_DOOR: STATE_DOOR;
 
 
 begin
@@ -106,15 +110,20 @@ port map(
 			);
 
 
---INIT-- EINMALIGES LADEN DES WATCHDOGTIMERS
+--INIT-- EINMALIGES LADEN DES 
+
+
 init: block
 begin
 	process(CLK)
 	begin
-		if(i_RST  <= '1' and STRB = '1') then    --RESET IST AN HIER IST ZEIT UM DIE VALUES ZU LADEN (HOFFENTLICH!)
-					VALUE <= o_DATA; --__WERDEN DIE DATEN VON ALLEINE GELADEN in o_DATA durch die Verdahrunt oder muss das 
+		REQUEST <= i_REQ_EXT or i_REQ_INT; --Auf ein "SIGNAL" bringen !
+	
+		--TO-DO: RICHTIGES LADEN DER VALUE
+		--if(i_RST  <= '1' and STRB = '1') then    --RESET IST AN HIER IST ZEIT UM DIE VALUES ZU LADEN (HOFFENTLICH!)
+			--		DATA_BUS <= o_DATA; --__WERDEN DIE DATEN VON ALLEINE GELADEN in o_DATA durch die Verdahrunt oder muss das 
 					--selber getan werden? 
-		end if;
+		--end if;
 	end process;
 end block init;
 	
@@ -144,30 +153,121 @@ begin
 end block Takt_teilung;
 
 
+zustandsautomat: block
 
+signal STAY_TRUE: STD_LOGIC := '1'; --Initialisiert darauf, dass der Fahrstuhl am Anfang im Stilltstand ist
+begin
 
---ZUSTANDSAUTOMAT - bitte geld einwerfen. --muss noch definiert werden wann er überhaupt in das ding reinp
+--ZUSTANDSAUTOMAT  --muss noch definiert werden wann er überhaupt in das ding reinp
 --springen kann, also den init
 
-Drive: process(i_REQ_EXT, i_REQ_INT)
+--I_POS setzt immer nur ein Bit!
+Drive: process(REQUEST)
 begin
+
 	case STATE is
-		when stay => if (i_REQ_EXT = i_POS or i_REQ_INT = i_POS) then NEXT_ST <= stay;
+		
+		
+								  --würde gehen, wenn es nur einmalige Anforderung gebe!
+		when drive_up => if(REQUEST > i_POS) then NEXT_ST <= drive_up; 
+				elsif((REQUEST and i_POS) = i_POS) then NEXT_ST <= stay; 
+				
+				STAY_TRUE <= '1'; --vom hochfahren in den Stillstand
+				
+		end if; --weiter hoch fahren beim hoch fahren
+ 		
+		when drive_down => if(REQUEST < i_POS) then NEXT_ST <= drive_down;  --weiter runter fahren beim runter fahren
+			elsif((REQUEST and i_POS) = i_POS) then NEXT_ST <= stay; 
+		end if;
+			
+		--WAS IST WENN EINE ANFRAGE VON UNTEN/OBEN KOMMT ABER GLEICHZEITIG SOLL WEITERHIN NOCH OFFEN BLEIBEN
+		when stay => if ((REQUEST and i_POS) = i_POS) then NEXT_ST <= stay; --stehen bleiben beim stehen bleiben
+						  elsif(REQUEST < i_POS) then NEXT_ST <= drive_up; --von stehen bleiben zu hoch fahren
+							elsif(REQUEST < i_POS) then NEXT_ST <= drive_down; --von stehen bleiben zu runter fahren
 		
 		end if;
+	
+	
 	end case;
 end process;
 
-DRIVE_RES: process(STATE)
-begin
-	case STATE is
-		when stay => i_DOOR_STAT <= "00";
-		end case;
 
+--Zustandsautomat 2 für die Tür
+
+--type STATE_DOOR is(door_openening, door_closing, door_open);
+--signal DOOR,NEXT_DOOR: STATE_DOOR;
+
+door: process(STAY_TRUE) --jedesmal wenn der STATE sich ändert hier rein gehen
+
+begin
+		if(STAY_TRUE = '1') then --Wenn Fahrstuhl im zustand STILLSTEHEND
+			case DOOR is
+			
+				when door_shut => if((REQUEST and i_POS) = i_POS) then NEXT_DOOR <= door_opening;
+										else NEXT_DOOR <= door_shut;
+				end if;
+				
+				when door_opening => if(i_DOOR_STAT = "00") then NEXT_DOOR <= door_open;
+											else NEXT_DOOR <= door_open;
+				
+				end if;
+				when door_open		=> if(((REQUEST and i_POS) /= i_POS) and DOG_OUT = '1') then NEXT_DOOR <= door_closing;
+											elsif((REQUEST and i_POS) = i_POS) then NEXT_DOOR <= door_opening;
+				
+				end if;
+				when door_closing => if(i_DOOR_STAT = "00") then NEXT_DOOR <= door_shut;
+											else NEXT_DOOR <= door_closing;
+				end if;
+	
+			end case;
+
+		end if;
+		
 end process;
 
 
 
+
+-- o_MOTOR_DIR : out  STD_LOGIC; --Richtungsangabe 1 -> hoch 0 -> runter
+-- o_MOTOR_EN : in  STD_LOGIC; --Motor an/aus
+
+
+
+
+--MOTORSTEUERUNG AUFZUG
+DRIVE_RES: process(STATE)
+begin
+	case STATE is
+				when stay => o_MOTOR_EN <= '0';
+				when drive_up => o_MOTOR_DIR <= '1'; o_MOTOR_EN <= '1';
+				when drive_down => o_MOTOR_DIR <= '0'; o_MOTOR_EN <= '0';
+		end case;
+end process;
+
+
+--MOTORSTEUERUNG TÜR
+--type STATE_DOOR is(door_openening, door_closing, door_open, door_shut);
+-- o_DOOR_DIR : in  STD_LOGIC; --Tür richtung 1 -> schließen , 0 -> öffnen
+-- o_DOOR_EN : in  STD_LOGIC); --Tür öffnen/schließen
+
+
+
+DRIVE_DOOR: process(DOOR)
+
+
+begin
+	case DOOR is
+		when door_shut => o_DOOR_EN <= '0';
+		when door_open => o_DOOR_EN <= '0';
+		when door_opening => o_DOOR_DIR <= '1'; o_DOOR_EN <= '1';
+		when door_closing => o_DOOR_DIR <= '0'; o_DOOR_EN <= '1';
+		end case;
+end process;
+end block zustandsautomat;
+
+
+
+--Kümmert sich um den zeitlichen Ablauf
 Tuer: block
 	
 begin
@@ -193,3 +293,4 @@ end block Tuer;
 
 
 end Behavioral;
+
